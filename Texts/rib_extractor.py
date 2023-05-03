179846948eb632db844f77656ca42d48f33e7320
fredfaya <edyfoottv@gmail.com>
2023-05-03 11:04:30 +0100

    feat: create a rib extractor
    the rib extractor functionality will allow us to retrieve any text that match the structure of an IBAN(RIB)

---
+++ Texts/rib_extractor.py
@@ -0,0 +1,14 @@
+import re
+
+
+def extract_rib(text: str) -> list[str]:
+    """Extracts a RIB from a text.
+
+    Args:
+        text (str): The text to extract the RIB from.
+
+    Returns:
+        str: The extracted RIB.
+    """
+    rib_regex = re.compile(r"[A-Z]{2}\d{2}\s(?:\d{4}\s){3,}\d*[0-9A-Za-z]?")
+    return rib_regex.findall(text)
