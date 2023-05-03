cc882949c38cded9421ac567b0a19dce765a0172
fredfaya <edyfoottv@gmail.com>
2023-05-03 15:17:30 +0100

    feat: create verify_infos_extracted function
    the goal of this function is to make sure that all the informations retrived by gpt exist in our file and that gpt have not create its own answers

--- Pipelines/pipeline_for_no_ocr_pdf.py
+++ Pipelines/pipeline_for_no_ocr_pdf.py
@@ -1,6 +1,7 @@
 from GPT.gpt import get_info_from_file
 from PDF_preprocessors.pdf_to_txt_convertor import convert_pdf_to_txt
 from Texts.rib_extractor import extract_rib
+from Texts.info_extracted_verification import verify_infos_extracted
 
 
 def pipeline_for_no_ocr_pdf(file_path):
@@ -15,6 +16,9 @@ def pipeline_for_no_ocr_pdf(file_path):
     infos = get_info_from_file(file_path)
     # extract RIB
     infos['RIB'] = extract_rib(txt_text)
+    # verify that all the information extracted are present in the file
+    infos = verify_infos_extracted(infos, file_path)
+
     return infos
 
 
---
+++ Texts/info_extracted_verification.py
@@ -0,0 +1,28 @@
+import re
+
+from PDF_preprocessors.pdf_to_txt_convertor import convert_pdf_to_txt
+
+
+def verify_infos_extracted(infos: dict, path_to_file: str) -> dict:
+    """
+    Verify that all the information extracted are present in the file.
+    :param path_to_file:
+    :param infos: dictionary with extracted information
+    :return: dictionary with verified information
+    """
+
+    text = convert_pdf_to_txt(path_to_file)
+    for key, value in infos.items():
+        if not isinstance(value, list):
+            values = re.split(r"\s|,|\.", value)
+            for item in values:
+                if item not in text:
+                    infos[key] = 'unknown'
+                    break
+        else:
+            for item in value:
+                if item not in text:
+                    infos[key] = 'unknown'
+                    break
+
+    return infos
